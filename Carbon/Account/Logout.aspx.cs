2a3686dfba7e53038f038feb3daa57dfb1fdc188
Eyob Birhanemeskel Ayalew <ebm-ai@users.noreply.github.com>
2024-03-08 01:36:38 +0300

    Update

---
+++ Carbon/Account/Logout.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Account_Logout : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        HttpContext.Current.Session.Clear();
+        HttpContext.Current.Session.Abandon();
+        HttpContext.Current.Response.Cookies.Add(new HttpCookie("ASP.NET_SessionId", ""));
+        Response.Redirect("~/Default.aspx");
+    }
+}
