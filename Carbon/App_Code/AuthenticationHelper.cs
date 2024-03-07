a5ebb6e380331d4879c9a7e34d22e736d6dd12ec
= <=>
2024-03-08 00:32:31 +0300

    logout section

--- Carbon/App_Code/AuthenticationHelper.cs
+++ Carbon/App_Code/AuthenticationHelper.cs
@@ -8,14 +8,8 @@ namespace Carbon {
     {
         public static void Logout()
         {
-            FormsAuthentication.SignOut(); // Sign out the user
-            var cookie = HttpContext.Current.Request.Cookies[FormsAuthentication.FormsCookieName];
-            if (cookie != null)
-            {
-                cookie.Expires = DateTime.Now.AddDays(-1);
-                HttpContext.Current.Response.Cookies.Add(cookie);
-            }
-            HttpContext.Current.Session.Abandon(); // Abandon the session
+            FormsAuthentication.SignOut();
+            //FormsAuthentication.RedirectToLoginPage();
         }
     }
 }
