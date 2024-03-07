c3b00bb02bf2660d5cb1e653615a8a05fe17ab91
= <=>
2024-03-07 20:02:17 +0300

    add authhelper

---
+++ Carbon/App_Code/AuthenticationHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Security;
+using System.Web;
+using System;
+
+
+namespace Carbon { 
+    public static class AuthenticationHelper
+    {
+        public static void Logout()
+        {
+            FormsAuthentication.SignOut(); // Sign out the user
+            var cookie = HttpContext.Current.Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (cookie != null)
+            {
+                cookie.Expires = DateTime.Now.AddDays(-1);
+                HttpContext.Current.Response.Cookies.Add(cookie);
+            }
+            HttpContext.Current.Session.Abandon(); // Abandon the session
+        }
+    }
+}
